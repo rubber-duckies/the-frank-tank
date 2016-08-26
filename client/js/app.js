49a639619d8fc3ee48fefa8b6802010770d2c306
Robert Ferguson <rob@rewferguson.com>
2016-08-26 15:28:03 -0500

    code cleanup

--- client/js/app.js
+++ client/js/app.js
@@ -1,17 +1,45 @@
-var totalTime;
+let player;
+let totalTime;
+let seek = false;
+let offset = 0;
+
+// elements
+const playHead = document.getElementById('playHead');
+const timeline = document.getElementById('timeline');
+const controls = document.getElementById('playerControls');
+const firstScriptTag = document.getElementsByTagName('script')[0];
+const tag = document.createElement('script');
 
 $(document).foundation();
 
-// This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+tag.src = 'https://www.youtube.com/iframe_api';
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.mute();
+  totalTime = event.target.getDuration();
+}
+
+// The API calls this function when the player's state changes.
+// The function indicates that when playing a video (state=1),
+// the player should play for six seconds and then stop.
+function onPlayerStateChange(event) {
+  if (event.data === YT.PlayerState.PLAYING) {
+    window.setInterval(() => {
+      const percent = (event.target.getCurrentTime()/totalTime)*100;
+
+      //var marker = document.getElementById('marker');
+      if (!seek) {
+        playHead.style.left = `${percent}%`;
+      }
+    }, 1000);
+  }
+}
 
 // This function creates an <iframe> (and YouTube player)
 // after the API code downloads.
-var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
@@ -26,79 +54,36 @@ function onYouTubeIframeAPIReady() {
       showinfo: 0, // hide video title
     },
     events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     }
   });
 }
 
-// The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-  event.target.mute();
-  totalTime = event.target.getDuration();
-}
-
-// The API calls this function when the player's state changes.
-// The function indicates that when playing a video (state=1),
-// the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    window.setInterval(function() {
-      var percent = (event.target.getCurrentTime()/totalTime)*100;
-
-      //var marker = document.getElementById('marker');
-      if (!seek) {
-        playHead.style.left = percent+'%';
-      }
-    }, 1000);
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
-
-
-
-
-
-// elements
-var playHead = document.getElementById('playHead');
-var timeline = document.getElementById('timeline');
-var controls = document.getElementById('playerControls');
-var info = document.getElementById('info');
-
-var seek = false;
-var offset = 0;
-
-playHead.addEventListener('mousedown', function(e) {
+playHead.addEventListener('mousedown', () => {
   seek = true;
-  //player.pauseVideo();
+  // player.pauseVideo();
 });
 
-playHead.addEventListener('mouseup', function(e) {
+playHead.addEventListener('mouseup', () => {
   seek = false;
-  var seekTime = player.getDuration() * offset;
-  console.log(player.getDuration() + ' * ' + offset + ' = ' + seekTime);
+  const seekTime = player.getDuration() * offset;
   player.seekTo(seekTime, true);
 });
 
-controls.addEventListener('mousemove', function(e) {
-  var userOffset = (e.clientX - (timeline.offsetLeft + controls.offsetLeft));
+controls.addEventListener('mousemove', (e) => {
+  const userOffset = (e.clientX - (timeline.offsetLeft + controls.offsetLeft));
   offset = (userOffset) / timeline.offsetWidth;
-  console.log(offset);
   if (offset > 1) {
     offset = 1;
   }
-  
+
   if (offset < 0) {
     offset = 0;
   }
-  
-  if (seek) { 
-    var offsetString = ((offset) * 100) +'%';
+
+  if (seek) {
+    const offsetString = `${(offset * 100)}%`;
     playHead.style.left = offsetString;
-    info.innerHTML = offsetString;
   }
 });
