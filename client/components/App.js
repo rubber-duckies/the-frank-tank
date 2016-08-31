a2917d2679836d8d06a9617d85cb840a315116ad
Robert Ferguson <rob@rewferguson.com>
2016-08-31 17:50:33 -0500

    added the background image and some additional styles

--- client/components/App.js
+++ client/components/App.js
@@ -7,14 +7,49 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       user: 'Frank',
+      background: '',
+      videos: [],
     };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:8000/channel/1',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify({
+        channel_id: 1,
+        user_id: 1,
+      }),
+    }).then(data => {
+      console.log('app', data);
+      this.setState({ 
+        background: data.background,
+        videos: data.videos,
+      });
+      $('body').css('background-image', `url(${this.state.background})`);
+    });
   }
 
   render() {
     return (
       <div>
-        <NavBar />
-        <PlayerWindow />
+        <header>
+          <div className="container">
+            <div className="row column">
+              <h1>The Frank Tank</h1>
+              <NavBar />
+            </div>
+          </div>
+        </header>
+
+        <div className="container">
+          <div className="row column">
+            <PlayerWindow videos={this.state.videos} />
+          </div>
+        </div>
       </div>
     );
   }
