f24a149212548666dbaa0c4ba74545e40bdd9dd8
Ryan Walter <r.walter215@gmail.com>
2016-08-30 23:21:25 -0500

    Have video player functionality with React Components

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as Nav from '../models/navigation.js';
+import { changeChannel } from '../models/navModel';
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -14,14 +14,14 @@ export default class NavBar extends React.Component {
       <ul className="dropdown menu" data-dropdown-menu>
         <li><a href="/">Home</a></li>
         <li className="is-dropdown-submenu-parent">
-          <a href="/">Channels</a>
+          <a>Channels</a>
           <ul className="menu">
-            <li><a href="/channel">Sea</a></li>
-            <li><a href="/channel">Air</a></li>
-            <li><a href="/channel">Land</a></li>
+            <li><a href="/channel/3">Sea</a></li>
+            <li><a href="/channel/2">Air</a></li>
+            <li><a href="/channel/1">Land</a></li>
           </ul>
         </li>
-        <li><a href="/">Logout</a></li>
+        <li><button onClick={changeChannel}>Logout</button></li>
       </ul>
     );
   }
--- client/components/PlayerWindow.js
+++ client/components/PlayerWindow.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+import { playerInit, onReady, onStateChange } from '../models/videoModel.js';
 
 export default class PlayerWindow extends React.Component {
   constructor(props) {
@@ -8,15 +10,42 @@ export default class PlayerWindow extends React.Component {
     };
   }
 
-  render() {
-    return (
-      <div>This is {this.state.user}'s PlayerWindow
-        <div className="flex-video widescreen">
-          <div id="player" />
-        </div>
+  // componentWillMount() {
+    
+  // }
 
+  componentDidMount() {
+    playerInit();
+  }
+
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters 
+        controls: 0, // hide player controls
+        // start: 10, // set player start time
+        // end: 20, // set player end time
+        iv_load_policy: 3, // hide annotations
+        rel: 0, // hide related videos
+        showinfo: 0, // hide video title
+      },
+    };
+
+    return (
+      <div>
+        <div className="flex-video widescreen">
+          <YouTube
+            videoId="RGXraFHbwD8"
+            opts={opts}
+            onReady={onReady}
+            onStateChange={onStateChange}
+            className="player"
+          />
+        </div>
         <section className="player-controls" id="playerControls">
           <div className="timeline" id="timeline">
+            <div id="moments"></div>
             <div className="playHead" id="playHead" />
           </div>
         </section>
@@ -24,4 +53,5 @@ export default class PlayerWindow extends React.Component {
       </div>
     );
   }
+
 }
