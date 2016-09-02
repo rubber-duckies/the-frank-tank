a3a29ed0d3f4e2bd6ddb97bc61b2394b589559a3
francistoto <fbt.github@gmail.com>
2016-09-02 13:06:35 -0500

    Refactored channel changing functionality, made menu clickable.

--- client/models/navModel.js
+++ client/models/navModel.js
@@ -1,4 +1,23 @@
-export function changeChannel() {
-  console.log('Great Success!');
-}
+import $ from './lib/jquery';
 
+const NavModel = {};
+
+NavModel.changeChannel = (channelId) => {
+  console.log('Great Success! Channel ID: ', channelId);
+  const channelUrl = `http://localhost:8000/channel/${
+    channelId === 1 ||
+    channelId === 2 ||
+    channelId === 3
+    ? channelId
+    : 'default'
+  }`;
+  return $.ajax({
+    url: channelUrl,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export default NavModel;
