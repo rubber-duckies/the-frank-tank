ee2161bc73b17a056bd0ee381c55d146dafb55b9
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-06 16:36:54 -0500

    updates knexfile to use connection object with ENV variables

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,12 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: process.env.DATABASE_URL || 'greenfield_vplayer',
+      // database: process.env.DATABASE_URL || 'greenfield_vplayer',
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      ssl: true
     },
   },
 
