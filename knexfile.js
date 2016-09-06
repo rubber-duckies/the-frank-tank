6d504bcd8a470806a4e6ae03a0b1be6b79df1de6
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-06 16:38:32 -0500

    Fixes env in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -6,10 +6,10 @@ module.exports = {
     client: 'postgresql',
     connection: {
       // database: process.env.DATABASE_URL || 'greenfield_vplayer',
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      database: DB_NAME,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       ssl: true
     },
   },
