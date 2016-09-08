22789bbdd5a32f015bcd266bbe3f8964baecc7d8
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-08 11:18:01 -0500

    Adds an iterator to mixtapeModel for seeking through video easily

--- client/models/mixtapeModel.js
+++ client/models/mixtapeModel.js
@@ -20,4 +20,47 @@ MixtapeModel.getVideoLikesByUser = function (userId) {
   });
 };
 
+MixtapeModel.getMomentIteratorByUser = function (userId) {
+  return MixtapeModel.getVideoLikesByUser(userId)
+    .then(function (likes) {
+      return createMomentIterator(likes);
+    })
+    .fail(function (err) {
+      console.log(err);
+      return null
+    });
+}
+
+function createMomentIterator (likes) {
+  var currentVideo = -1;
+  var currentMoment = -1;
+  var iterator = {
+    // Checks if there is another video URL
+    hasNextVideo: function () {
+      return currentVideo + 1 < likes.length;
+    },
+    // Returns next video URL
+    nextVideo: function () {
+      if (!iterator.hasNextVideo()) {
+        return null;
+      }
+      currentVideo += 1;
+      currentMoment = -1;
+      return likes[currentVideo].url;
+    },
+    hasNextMoment: function () {
+      return likes[currentVideo] &&
+        currentMoment + 1 < likes[currentVideo].moments.length;
+    },
+    nextMoment: function () {
+      if (!iterator.hasNextMoment()) {
+        return null;
+      }
+      currentMoment += 1;
+      return likes[currentVideo].moments[currentMoment];
+    }
+  };
+  return iterator;
+}
+
 export default MixtapeModel;
