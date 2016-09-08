569e1e31774008fe21919ff54b59886460da68af
Christopher Kiel <no-fi@users.noreply.github.com>
2016-09-08 17:09:57 -0500

    Adds a route for /mixtape that uses logged in user id

--- server/Server.js
+++ server/Server.js
@@ -408,11 +408,21 @@ app.get('/videos/:id', (req, res) => {
 /*
   Endpoint for Mixtape --> Returns JSON Array of likes by a userId
 */
+app.all('/testPage', isLoggedIn);
+app.get('/mixtape', (req, res) => {
+  db.getVideoLikesByUser(req.user.id)
+    .then(likes => res.status(200).send(likes))
+    .catch(err => {
+      console.log('ROUTE: /mixtape ', err);
+      res.status(400).send();
+    });
+});
+
 app.get('/mixtape/user/:id', (req, res) => {
   db.getVideoLikesByUser(req.params.id)
     .then(likes => res.status(200).send(likes))
     .catch(err => {
-      console.log('ROUTE: /mixtape/user/:id ', err);
+      console.log('ROUTE: /mixtape ', err);
       res.status(400).send();
     });
 });
